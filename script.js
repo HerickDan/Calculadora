9fa7d23e22590361c15ee0b760737099d5eb239c
HerickDev <107524132+HerickDev@users.noreply.github.com>
2022-08-27 23:21:59 -0300

    Estrutura, script e estilização

---
+++ script.js
@@ -0,0 +1,17 @@
+function apaga(){
+    var resultado =  document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
+function clean(){
+    document.getElementById('resultado').innerHTML = ""
+}
+function insert(num){
+    var numero = document.getElementById('resultado').innerHTML
+    document.getElementById('resultado').innerHTML = numero+num
+}
+function calcula(){
+    var resultado=document.getElementById("resultado").innerHTML;
+    if (resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }
+}
